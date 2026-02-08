2b8a2172011f27a3c412fd9e5f39c2c11dffb079
TheQueCode <thequecode@gmail.com>
2026-02-07 20:51:41 -0600

    functions

--- script.js
+++ script.js
@@ -32,3 +32,62 @@ function kiloToMiles(kilos){
 
 // Calculate the sum of numbers within an array
 
+// different ways
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// for loop
+
+function sumArr(array){
+  let sum = 0;
+  for(let i = 0; i < array.length; i++){
+    sum += array[i]
+  }
+  return sum;
+}
+
+console.log(sumArr(numbers));
+
+// for...of loop
+
+function sumOfArray(array){
+  let sum = 0;
+  for (const num of array){
+    sum += num;
+  }
+  return sum;
+}
+
+console.log(sumOfArray(numbers));
+
+// .forEach
+
+function sumForEach(array){
+  let sum = 0;
+  array.forEach(number => { sum += number; });
+  return sum;
+}
+
+console.log(sumForEach(numbers));
+
+// reduce
+
+const sum = numbers.reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue;
+}, 0);
+
+console.log(sum);
+
+// reduce with arrow function
+
+const sumAgain = numbers.reduce((acc, cur) => acc + cur, 0);
+
+console.log(sumAgain);
+
+// Create a function that reverses an array
+
+function reverse(array){
+  console.log(array);
+}
+
+reverse(numbers);
